9b54a93aeff0c30af00b3ce31babc0f228cb6b7c
knd <delend89@gmail.com>
2023-03-30 22:12:49 -0300

    single_consult

--- helpers/format_form_data.py
+++ helpers/format_form_data.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import datetime
 
 def format_form_data():
     format_form_data_cmps()
@@ -9,34 +10,34 @@ def format_form_data():
 
 
 def format_form_data_complain():
-    st.session_state.client['complain'] = 1 if st.session_state.client['complain'] else 0
+    st.session_state.client['Complain'] = 1 if st.session_state.client['Complain'] else 0
 
 
 def format_form_data_date():
-    date = st.session_state.client['registered']
+    date = st.session_state.client['Time_Customer']
     today = datetime.date.today()
     date = today - date
-    st.session_state.client['registered'] = date.days / 365.25
+    st.session_state.client['Time_Customer'] = date.days / 365.25
     
 def format_form_data_cmps():
-    st.session_state.client['Cmp1'] = 0
-    st.session_state.client['Cmp2'] = 0
-    st.session_state.client['Cmp3'] = 0
-    st.session_state.client['Cmp4'] = 0
-    st.session_state.client['Cmp5'] = 0
+    st.session_state.client['AcceptedCmp1'] = 0
+    st.session_state.client['AcceptedCmp2'] = 0
+    st.session_state.client['AcceptedCmp3'] = 0
+    st.session_state.client['AcceptedCmp4'] = 0
+    st.session_state.client['AcceptedCmp5'] = 0
 
-    if 'Cmp1' in st.session_state.client['cmps']:
-        st.session_state.client['Cmp1'] = 1
+    if 'AcceptedCmp1' in st.session_state.client['cmps']:
+        st.session_state.client['AcceptedCmp1'] = 1
 
-    if 'Cmp2' in st.session_state.client['cmps']:
-        st.session_state.client['Cmp2'] = 1
+    if 'AcceptedCmp2' in st.session_state.client['cmps']:
+        st.session_state.client['AcceptedCmp2'] = 1
 
-    if 'Cmp3' in st.session_state.client['cmps']:
-        st.session_state.client['Cmp3'] = 1
+    if 'AcceptedCmp3' in st.session_state.client['cmps']:
+        st.session_state.client['AcceptedCmp3'] = 1
 
-    if 'Cmp4' in st.session_state.client['cmps']:
-        st.session_state.client['Cmp4'] = 1
+    if 'AcceptedCmp4' in st.session_state.client['cmps']:
+        st.session_state.client['AcceptedCmp4'] = 1
 
-    if 'Cmp5' in st.session_state.client['cmps']:
-        st.session_state.client['Cmp5'] = 1
+    if 'AcceptedCmp5' in st.session_state.client['cmps']:
+        st.session_state.client['AcceptedCmp5'] = 1
     st.session_state.client.pop('cmps')
